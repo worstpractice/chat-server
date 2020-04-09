785dc54603db58eab9a064339e38322307572935
Erik Nygren <erik.nygren@appliedtechnology.se>
2020-04-10 00:47:42 +0200

    cleanup console logging

--- src/listeners/websocket/client/message.ts
+++ src/listeners/websocket/client/message.ts
@@ -4,7 +4,6 @@ import { deserialize } from "../../../messages/deserialize.js";
 import { acceptUsername, rejectUsername } from "../../../messages/response.js";
 import { serialize } from "../../../messages/serialize.js";
 import { nay } from "../../../utils/nay.js";
-import { say } from "../../../utils/say.js";
 import { isInboxChatMessage, isInboxUsername } from "../../../utils/typePredicates.js";
 import { yay } from "../../../utils/yay.js";
 import { hasValidUsername } from "./metadata/hasValidUsername.js";
@@ -12,10 +11,7 @@ import { takenUsernames } from "./metadata/takenUsernames.js";
 
 function handleClientMessage(this: WebSocket, data: WebSocket.Data): void {
   yay(`Client message recieved!`);
-  say(`Data: ${data}`);
   const message = deserialize(data);
-
-  console.log(hasValidUsername);
 
   if (isInboxUsername(message)) {
     if (takenUsernames.has(message.username)) {
