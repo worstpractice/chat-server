912215102048da2533b0e9872c7dd7495dbbb90f
Erik Nygren <erik.nygren@appliedtechnology.se>
2020-04-11 15:13:08 +0200

    run prettier

--- src/time/timeConversion.ts
+++ src/time/timeConversion.ts
@@ -6,4 +6,8 @@ const divideBy60 = (unitOfTime: number): number => {
   return Math.floor(unitOfTime / 60);
 };
 
-export { divideBy60 as secondsToMinutes, divideBy60 as minutesToHours, divideBy24 as hoursToDays };
+export {
+  divideBy60 as secondsToMinutes,
+  divideBy60 as minutesToHours,
+  divideBy24 as hoursToDays,
+};
